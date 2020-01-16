b37d4288fa5e240f80c1b8e383162ba4e95e2f8b
Paweł Szostak <szostak_pawel@outlook.com>
2020-01-16 12:50:31 +0100

    Cleared initial struture

---
+++ src/Page.js
@@ -0,0 +1,12 @@
+import './Page.css';
+import React, { Component } from 'react'
+
+class Page extends Component {
+  render() {
+    return (
+      <h1>works</h1>
+    )
+  }
+}
+
+export default Page;

5b9a89958a7dcf14a9c36d7e60d8e4189bf6fa28
Paweł Szostak <szostak_pawel@outlook.com>
2020-01-19 15:49:24 +0100

    Added basename

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Page from './Page';
-ReactDOM.render(<Router><Page/></Router>, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><Page/></Router>, document.getElementById('root'));

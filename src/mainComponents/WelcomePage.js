521a705bd68b86af3737cb46e561d4eab1478b89
Paweł Szostak <szostak_pawel@outlook.com>
2020-03-21 07:48:42 +0100

    Corrections

--- src/mainComponents/WelcomePage.js
+++ src/mainComponents/WelcomePage.js
@@ -5,22 +5,14 @@ import { CSSPlugin } from 'gsap/CSSPlugin'
 
 const WelcomePage = () => {
     gsap.registerPlugin(CSSPlugin);
-    let h1 = useRef(null);
     let section = useRef(null);
-    let btn = useRef(null);
     useEffect(()=>{
-        TweenMax.to(h1, {duration: 2.5, opacity: 1});
         TweenMax.to(section, {duration: 2, opacity: 1});
-        TweenMax.to(btn, {duration: 3, opacity: 1});
-    },[]);
+    });
     return (
-        <section 
-        style={{opacity: 0}}
-        className="welcome-page" 
-        ref={el => {section = el}}
-        >
-            <h1 ref={el => {h1 = el}}>Are you ready to discover some amazing travel destinations?</h1>
-            <Link to="/australia" className="explore-btn" ref={el => {btn = el}}>Discover</Link>
+        <section style={{opacity: 0}} className="welcome-page" ref={el => {section = el}}>
+            <h1>Are you ready to discover some amazing travel destinations?</h1>
+            <Link to="/australia" className="explore-btn">Discover</Link>
         </section>
     )
 }
--- src/structureComponents/CurrentPage.js
+++ src/structureComponents/CurrentPage.js
@@ -1,38 +1,39 @@
-import React, {Component} from 'react'
+import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom';
 
-class CurrentPage extends Component{
-    componentDidMount(){
-      const currentA = [...document.querySelectorAll('.current-page a')];
-      currentA.forEach(a => a.classList.remove('active'))
-      if(this.props.current.includes("australia")){
-        const filtered = currentA.filter(a => a.href.includes('australia'));
-        filtered.forEach(a => a.classList.add('active'))
-      }else if(this.props.current.includes("vietnam")){
-        const filtered = currentA.filter(a => a.href.includes('vietnam'));
-        filtered.forEach(a => a.classList.add('active'))
-      }else if(this.props.current.includes("indonesia")){
-        const filtered = currentA.filter(a => a.href.includes('indonesia'));
-        filtered.forEach(a => a.classList.add('active'))
-      }else if(this.props.current.includes("thailand")){
-        const filtered = currentA.filter(a => a.href.includes('thailand'));
-        filtered.forEach(a => a.classList.add('active'))
-      }else if(this.props.current.includes("malaysia")){
-        const filtered = currentA.filter(a => a.href.includes('malaysia'));
-        filtered.forEach(a => a.classList.add('active'))
-      }
+const CurrentPage = props => {
+  const current = props.current;
+
+  useEffect(()=>{
+    const allA = [...document.querySelectorAll('.current-page a')];
+    allA.forEach(a => a.classList.remove('active'));
+    if(current.includes("australia")){
+      filtering(allA, 'australia')
+     }else if(current.includes("vietnam")){
+      filtering(allA, 'vietnam')
+     }else if(current.includes("indonesia")){
+       filtering(allA, 'indonesia')
+     }else if(current.includes("thailand")){
+      filtering(allA, 'thailand')
+    }else if(current.includes("malaysia")){
+      filtering(allA, 'malaysia')
     }
-    render(){
-      return (
-          <div className="current-page">
-            <Link to="/australia"><i className="fas fa-circle"></i></Link>
-            <Link to="/vietnam"><i className="fas fa-circle"></i></Link>
-            <Link to="/indonesia"><i className="fas fa-circle"></i></Link>
-            <Link to="/thailand"><i className="fas fa-circle"></i></Link>
-            <Link to="/malaysia"><i className="fas fa-circle"></i></Link>
-          </div>
-      )
-    }
+  })
+
+  const filtering = (anchors, destination) => {
+    const filtered = anchors.filter(a => a.href.includes(destination));
+    filtered.forEach(a=>a.classList.add('active'))
+  }
+
+  return (
+    <div className="current-page">
+      <Link to="/australia"><i className="fas fa-circle"></i></Link>
+      <Link to="/vietnam"><i className="fas fa-circle"></i></Link>             
+      <Link to="/indonesia"><i className="fas fa-circle"></i></Link>
+      <Link to="/thailand"><i className="fas fa-circle"></i></Link>
+      <Link to="/malaysia"><i className="fas fa-circle"></i></Link>
+    </div>
+  )
 }
 
 export default CurrentPage
